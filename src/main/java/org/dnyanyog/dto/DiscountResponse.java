71c8c38c3ea1f4c12742dc4680a4b036a9a58524
_saurabhh_sawant <saurabhsawant2001@gmail.com>
2024-04-03 23:14:17 +0530

    feat: Implement customer service
    * Implement google java formatter
    * Add pre-commit-config.yaml file

---
+++ src/main/java/org/dnyanyog/dto/DiscountResponse.java
@@ -0,0 +1,17 @@
+package org.dnyanyog.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DiscountResponse {
+
+  private int discountPercent;
+
+  public int getDiscountPercent() {
+    return discountPercent;
+  }
+
+  public void setDiscountPercent(int discountPercent) {
+    this.discountPercent = discountPercent;
+  }
+}
